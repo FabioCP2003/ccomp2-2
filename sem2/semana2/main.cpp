b3758accc2caac2065b41a9d1f2b09396d8a1d6e
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-08-22 08:18:37 -0500

    mod sem2

--- sem2/semana2/main.cpp
+++ sem2/semana2/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int val1 = 10;
+    int val2 = 5;
+    int res = val1 + val2;
+    cout << "El resultado es :" << res << endl;
     return 0;
 }
