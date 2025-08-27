9e971e4054b2f7e1e33284f80df5769aa0c58169
Alumno B303 <Alumno-B303@UCSP.AULAB>
2025-08-27 07:35:49 -0500

    SRMD y comentarios

--- sem2/semana2/main.cpp
+++ sem2/semana2/main.cpp
@@ -4,9 +4,18 @@ using namespace std;
 
 int main()
 {
-    int val1 = 10;
-    int val2 = 5;
-    int res = val1 + val2;
-    cout << "El resultado es :" << res << endl;
+    int val1 = 18;
+    float val2 = 5;
+    int suma = val1 + val2;
+	int rest = val1 - val2;
+	int mult = val1 * val2;  // comentariooooo
+	float divi = val1 / val2;
+	/*  comentario mas grande
+	eeeee
+	eeeee*/
+    cout << "La suma es :" << suma << endl;
+	cout << "La resta es :" << rest << endl;
+	cout << "La multiplicacion es :" << mult << endl;
+	cout << "La division es :" << divi << endl;
     return 0;
 }
